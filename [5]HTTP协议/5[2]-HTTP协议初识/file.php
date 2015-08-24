ded0cdf66b990baceaa10d7ec06c18ac88847e40
shuFlower <1510276107@qq.com>
2015-08-24 20:45:44 +0800

    http socket

--- [5]HTTP协议/5[2]-HTTP协议初识/file.php
+++ [5]HTTP协议/5[2]-HTTP协议初识/file.php
@@ -5,7 +5,7 @@
  * Date: 2015/8/24
  * Time: 18:16
  *
- * 抓取页面信息：php程序模拟浏览器
+ * 抓取页面信息：php程序-模拟浏览器-请求数据
  */
 
 //file
@@ -22,21 +22,30 @@
 //print_r($result_array);
 
 
+//readfile()
+//$result = readfile('www.baidu.com');   //报错：Warning: readfile(www.baidu.com): failed to open stream
+$result = readfile('https://www.baidu.com');  //正确
+print_r($result);
+
+
+//curl方法
+
+
 
 //fsockopen
-$url = 'www.baidu.com';
-$fp = fsockopen($url, 443, $errno, $errstr, 30);
-if(!$fp)
-{
-    echo $errstr.':'.$errno;
-}
-else
-{
-    $out = "GET / HTTP/1.1\n\n";
-    fwrite($fp, $out);
-    while(!feof($fp))
-    {
-        echo fgets($fp, 128);
-    }
-    fclose($fp);
-}
+//$url = 'http://www.qq.com/';
+//$fp = fsockopen($url, 8080, $errno, $errstr, 30);
+//if(!$fp)
+//{
+//    echo $errstr.':'.$errno;
+//}
+//else
+//{
+//    $out = "GET / HTTP/1.0\n\n";
+//    fwrite($fp, $out);
+//    while(!feof($fp))
+//    {
+//        echo fgets($fp, 128);
+//    }
+//    fclose($fp);
+//}
